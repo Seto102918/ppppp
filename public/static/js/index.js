860c4118e33e43a5000807b81266381188bb498a
BINGCHILLING555 <96429329+BINGCHILLING555@users.noreply.github.com>
2022-05-25 18:33:23 +0700

    make it better

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,7 +1,9 @@
 import * as d3 from "https://cdn.skypack.dev/d3@7";
+// import * as anychart from "https://cdn.anychart.com/releases/8.10.0/js/anychart-base.min.js";
 import axios from 'https://cdn.skypack.dev/axios';
 
 console.log('js TOP')
+document.getElementById("p1").innerHTML = "world";
 
 const dataSet = async function getData() {
     return await axios.get('/api/data');
@@ -22,21 +24,37 @@ data.data.forEach(data => {
 });
 console.log(timeData)
 
-
-// const dataSet2 = async function getData2() {
-//     return await axios.get('/api/data2');
+// function getData() {
+//   return [
+//     ['1990',12],
+//     ['1991',14],
+//     ['1993',21],
+//     ['1994',21],
+//     ['1996',26],
+//     ['1998',26],
+//     ['2000',27],
+//     ['2002',31],
+//     ['2004',29],
+//     ['2006',31],
+//     ['2008',36],
+//     ['2010',41],
+//     ['2012',42],
+//     ['2014',48],
+//     ['2016',50],
+//     ['2018',57]
+//   ];
 // }
-// var data2  = await dataSet2()
-// var datastr2 = JSON.stringify(data2.data)
 
+// var dataSet = anychart.data.set(getData());
+// var seriesData = dataSet.mapAs({ x: 0, value: 1 });
+// var chart = anychart.line();
 
-// data2.data.forEach(data => {
-//     var hasil = timeParse(data.time)
-//     var obj = {"value": data.value, "time": hasil}
-//     timeData.push(obj)
-// });
-// console.log(data2)
+// chart.title('Acceptance of same-sex relationships in the US over the last 2 decades');
+// chart.yAxis().title('% of people who accept same-sex relationships');
 
+// var lineChart = chart.line(seriesData);
+// chart.container('container');
+// chart.draw();
 
 function createLinechart(data){
   var parentDiv = document.getElementById("my_dataviz");
